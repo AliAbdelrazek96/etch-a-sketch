a25613d75e20eb0a8291195936c0b619fcac756d
AliAhmed96 <129962715+AliAbdelrazek96@users.noreply.github.com>
2023-07-17 15:43:43 +0300

    make an adjustable grid

--- index.js
+++ index.js
@@ -6,10 +6,33 @@ const parentDiv = document.createElement('div')
 parentDiv.classList.add('parent-div');
 body.appendChild(parentDiv)
 
-//use a for loop to create 256 grid items! (16 x 16)
-for (let i = 1; i <= 256; i++) {
+
+
+const openPopupButton = document.getElementById('openPopup');
+
+openPopupButton.addEventListener('click', function() {
+    let gridSize;
+  do { gridSize = Number(prompt("How many squares per side?"))
+} while(isNaN(gridSize) || gridSize < 1 || gridSize > 100)
+  //store the entered number in a variable
+    //repeat the prompt as long as the value is NaN or < 1 or > 100
+ //remove the grid when new input is given
+    while (parentDiv.firstChild) {
+    parentDiv.removeChild(parentDiv.firstChild)
+ }
+
+ let gridItemWidth = 100/gridSize + '%';
+let gridItemHeight = 100/gridSize + '%';
+    // add grid items with a for loop
+   //use a for loop to create 256 grid items! (16 x 16)
+for (let i = 1; i <= gridSize * gridSize; i++) {
     const gridItem = document.createElement('div') // create a new div with each iteration
     gridItem.classList.add('grid-item');
+    gridItem.style.flexBasis = gridItemWidth;
     parentDiv.appendChild(gridItem);
 }
+
+
+
+})
 
